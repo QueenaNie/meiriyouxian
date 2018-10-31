d116811fdb6ac6af1277579f6756c756c6643565
QueenaNie <41670480+QueenaNie@users.noreply.github.com>
2018-10-31 17:03:20 +0800

    Update index.js

--- dayup/src/store/reducers/index.js
+++ dayup/src/store/reducers/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react"
 import {combineReducers} from 'redux';
 import * as Types from "../action-types";
-let initState={
+let initState1={
     carts:[],//存放购物车头部的数据,
     detSliders:[]//存放轮播图的数据
 
@@ -9,14 +9,14 @@ let initState={
 function home(state={},action) {
     return state;
 }
-function cart(state=initState,action) {
+function cart(state=initState1,action) {
     switch (action.type){
         case Types.JUMP_CART:
             return {...state,carts:action.payload}
     }
     return state;
 }
-function detailSlider(state=initState,action) {
+function detailSlider(state=initState1,action) {
     switch (action.type){
         case Types.DETAIL_SLIDER:
             return {...state,detSliders:action.payload}
